bea090ef000ebeaf24ca60223e715e3ef4468e36
Dustin Brickwood <dustinbrickwood204@gmail.com>
2025-04-30 11:33:01 -0500

    fix: fix sha mismatch from workflow (#689)

--- Formula/anvil-zksync.rb
+++ Formula/anvil-zksync.rb
@@ -6,20 +6,20 @@ class AnvilZksync < Formula
     on_macos do
       if Hardware::CPU.arm?
         url "https://github.com/matter-labs/anvil-zksync/releases/download/v#{version}/anvil-zksync-v#{version}-aarch64-apple-darwin.tar.gz"
-        sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+        sha256 "ba36f345f373a1c22625aa7979bb7f26604ba5ec523f4c6e54dc65389eb4fb07"
       else
         url "https://github.com/matter-labs/anvil-zksync/releases/download/v#{version}/anvil-zksync-v#{version}-x86_64-apple-darwin.tar.gz"
-        sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+        sha256 "00213600565a1b5e237b1651f69d4670e120cd628a59646a671ca6dc54c5143b"
       end
     end
   
     on_linux do
       if Hardware::CPU.arm?
         url "https://github.com/matter-labs/anvil-zksync/releases/download/v#{version}/anvil-zksync-v#{version}-aarch64-unknown-linux-gnu.tar.gz"
-        sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+        sha256 "bf0b6b3c3b06cabfafba02f8af59b363a9ae7aa17d64922252097f1a96195640"
       else
         url "https://github.com/matter-labs/anvil-zksync/releases/download/v#{version}/anvil-zksync-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-        sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+        sha256 "d5754b6666092e4b7d44caa253ef4c5d2a478c6f84a0efeead39ab51e47bdc4c"
       end
     end
   
